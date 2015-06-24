3a7d099aa9abe018803d5bb11137282da71e3590
Ron Waldon <jokeyrhyme@gmail.com>
2015-06-24 15:13:29 +1000

    configure Karma testing in PhantomJS and Firefox

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 'use strict';
 
-function JQPromise () { }
+// foreign modules
+
+var $ = require('jquery') || global.$;
+
+// this module
+
+function JQPromise (exec) {
+  var dfrd = new $.Deferred();
+  exec(dfrd.resolve, dfrd.reject);
+  return dfrd.promise();
+}
 
 module.exports = JQPromise;
--- test/index.js
+++ test/index.js
@@ -21,3 +21,78 @@ test('JQPromise', function (t) {
 
   t.end();
 });
+
+test('new JQPromise(); #resolve()', function (t) {
+  var jqp;
+
+  t.test('create a new JQPromise', function (tt) {
+    jqp = new JQPromise(function (resolve) {
+      setTimeout(function () {
+        resolve();
+        tt.end();
+      }, 0);
+    });
+  });
+
+  t.test('#then(...)', function (tt) {
+    jqp.then(function () {
+      tt.lengthOf(arguments, 0);
+      tt.end();
+    }, function (err) {
+      tt.error(err);
+      tt.end();
+    });
+  });
+
+  t.end();
+});
+
+test('new JQPromise(); #resolve("abc")', function (t) {
+  var jqp;
+
+  t.test('create a new JQPromise', function (tt) {
+    jqp = new JQPromise(function (resolve) {
+      setTimeout(function () {
+        resolve('abc');
+        tt.end();
+      }, 0);
+    });
+  });
+
+  t.test('#then(...)', function (tt) {
+    jqp.then(function (result) {
+      tt.equal(result, 'abc');
+      tt.end();
+    }, function (err) {
+      tt.error(err);
+      tt.end();
+    });
+  });
+
+  t.end();
+});
+
+test('new JQPromise(); #reject(new Error("abc"))', function (t) {
+  var jqp;
+
+  t.test('create a new JQPromise', function (tt) {
+    jqp = new JQPromise(function (resolve, reject) {
+      setTimeout(function () {
+        reject(new Error('abc'));
+        tt.end();
+      }, 0);
+    });
+  });
+
+  t.test('#then(...)', function (tt) {
+    jqp.then(function () {
+      tt.ok(false, 'resolve handler should not execute');
+      tt.end();
+    }, function (err) {
+      tt.ok(err);
+      tt.end();
+    });
+  });
+
+  t.end();
+});
